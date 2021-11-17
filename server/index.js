0aeedd809f7b7a7728c8d099626f547f2308870b
Johans <johansneirap@gmail.com>
2021-11-17 17:07:51 -0300

    configure github actions

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path')
 
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, '../client-react/build')))
+
 const db = require('./models');
+
+app.use
 
 // Routers
 const postRouter = require('./routes/Posts');
@@ -16,7 +21,7 @@ const usersRouter = require('./routes/Users');
 app.use('/auth', usersRouter);
 
 db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log('Server running on port 3001');
-  });
+    app.listen(3001, () => {
+        console.log('Server running on port 3001');
+    });
 });
